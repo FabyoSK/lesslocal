8f1c14e45811c4d6b4c20d453acb3c75c5221744
Fábio Alves <fabioalves.contacto@gmail.com>
2023-09-14 22:09:59 -0100

    #22: Symlink shared modules to node_modules

--- src/hooks/postdeploy.ts
+++ src/hooks/postdeploy.ts
@@ -1,39 +1,27 @@
-import { spawnSync } from 'node:child_process';
-
-import * as fs from 'node:fs';
+import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
 
 const hook = async function (): Promise<void> {
-  // console.log('[less] Removing dependencies removed from package.json...')
-
-  const packageJsonPath = path.join(process.cwd(), 'package.json');
-  const dependenciesToRemove: string[] = [];
-
   try {
-    // Read the package.json file asynchronously
-    const packageJsonContent = await fs.promises.readFile(packageJsonPath, 'utf8');
-    const packageJson = JSON.parse(packageJsonContent);
-
     // Get a list of directories in the './src/shared' directory
-    const files = await fs.promises.readdir('./src/shared', { withFileTypes: true });
+    const files = await fs.readdir(
+      path.join(process.cwd(), 'src', 'shared'),
+      { withFileTypes: true },
+    );
 
+    const symlinkLinks: Promise<void>[] = [];
     // Identify and collect directories to remove
     for (const file of files) {
       if (file.isDirectory() && file.name !== 'node_modules') {
-        dependenciesToRemove.push(file.name);
+        // remove symlink on node_modules
+        const nodeModulesPath = path.join(process.cwd(), 'node_modules', file.name);
+        symlinkLinks.push(
+          fs.unlink(nodeModulesPath).catch(),
+        );
       }
     }
 
-    // Remove the specified dependencies from packageJson
-    for (const dependency of dependenciesToRemove) {
-      delete packageJson.devDependencies[dependency];
-    }
-
-    const updatedPackageJsonContent = JSON.stringify(packageJson, null, 2);
-
-    // Write the updated package.json file asynchronously
-    await fs.promises.writeFile(packageJsonPath, updatedPackageJsonContent, 'utf8');
-    spawnSync('yarn', ['install', '--cwd', process.cwd()]);
+    await Promise.all(symlinkLinks);
   } catch (error) {
     console.error('An error occurred:', error);
   }
